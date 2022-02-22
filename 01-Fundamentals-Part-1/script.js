8351e04942ea5b867c2373a7ce07cb69c08434c7
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-22 14:46:37 +0200

    fundamentals of javascript

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+let firstName = "kenneth";
+console.log(firstName);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
