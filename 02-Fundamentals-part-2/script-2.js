4c05a747a7ef383c4477f5ce6f66199d6eebb736
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-23 13:20:25 +0200

    coding challenge #2

--- 02-Fundamentals-part-2/script-2.js
+++ 02-Fundamentals-part-2/script-2.js
@@ -157,3 +157,76 @@ const ages = [
   calcAged(years[years.length - 1]),
 ];
 console.log(ages);
+
+// Elements to insert at the end of the array
+// const friends = [`Micheal`, `Steven`, `Peter`];
+friends.push(`jay`);
+const newLenght = friends.push(`jay`);
+console.log(newLenght);
+
+// Elements to insert at the start of the array
+friends.unshift(`john`);
+console.log(friends);
+
+// Element to remove at the end of an array
+friends.pop(); // last
+friends.pop();
+
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+//Element to remove at the start of an array
+friends.shift();
+console.log(friends);
+
+//Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+console.log(friends.indexOf(`Steven`));
+console.log(friends.indexOf(`bob`));
+
+//Determines whether an array includes a certain element, returning true or false as appropriate.
+friends.push(23);
+console.log(friends);
+console.log(friends.includes(`Steven`));
+console.log(friends.includes(`bob`));
+console.log(friends.includes(23));
+// mostly used to write conditionals
+if (friends.includes(`Steven`)) {
+  console.log(`you have a friend called steven`);
+}
+// Coding Challenge #2
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array) �
+GOOD LUCK �*/
+// const calcTip = function(bill){
+//   if (bill >= 50 && bill <= 300){
+//     tip = bill * 0.2
+//   }else {
+//     tip = bill * 0.15
+//   }
+// }
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(calcTip(44));
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips, bills);
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+console.log(total);
