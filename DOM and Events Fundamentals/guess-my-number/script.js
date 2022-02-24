2b9bbc7fd9fdf9cb6ec5008897a3e6fb7fdce24f
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-24 21:20:07 +0200

    added the DRY principle
    DRY stands for don't repeat yourself

--- DOM and Events Fundamentals/guess-my-number/script.js
+++ DOM and Events Fundamentals/guess-my-number/script.js
@@ -8,12 +8,16 @@ document.querySelector(`.guess`).value = 45;*/
 let score = 20;
 let highScore = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const displayMessage = function (message) {
+  document.querySelector(`.message`).textContent = message;
+};
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
   //when there is no input
   if (!guess) {
-    document.querySelector(`.message`).textContent = `please input a number`;
+    displayMessage(`please input a number`);
+    // document.querySelector(`.message`).textContent = `please input a number`;
     //when player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `correct answer`;
@@ -26,27 +30,40 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     }
 
     //when guess is too high
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(`.message`).textContent = `📈 Too high!`;
+      displayMessage(guess > secretNumber ? `📈 Too high!` : `📉 Too low!`);
+      // document.querySelector(`.message`).textContent =
+      //   guess > secretNumber ? `📈 Too high!` : `📉 Too low!`;
       score--;
       document.querySelector(`.score`).textContent = score;
     } else {
-      document.querySelector(`.message`).textContent = `you lost the game`;
-      document.querySelector(`.score`).textContent = 0;
-    }
-    //when guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(`.message`).textContent = `📉 Too low!`;
-      score--;
-      document.querySelector(`.score`).textContent = score;
-    } else {
-      document.querySelector(`.message`).textContent = `you lost the game`;
+      displayMessage(`you lost the game`);
+      //document.querySelector(`.message`).textContent = `you lost the game`;
       document.querySelector(`.score`).textContent = 0;
     }
   }
 });
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(`.message`).textContent = `📈 Too high!`;
+//       score--;
+//       document.querySelector(`.score`).textContent = score;
+//     } else {
+//       document.querySelector(`.message`).textContent = `you lost the game`;
+//       document.querySelector(`.score`).textContent = 0;
+//     }
+//     //when guess is too low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(`.message`).textContent = `📉 Too low!`;
+//       score--;
+//       document.querySelector(`.score`).textContent = score;
+//     } else {
+//       document.querySelector(`.message`).textContent = `you lost the game`;
+//       document.querySelector(`.score`).textContent = 0;
+//     }
+//   }
 //Coding Challenge #1
 /*
 Implement a game rest functionality, so that the player can make a new guess!
@@ -63,7 +80,8 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
   score = 20;
   document.querySelector(`.score`).textContent = score;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(`.message`).textContent = `Start guessing...`;
+  displayMessage(`Start guessing...`);
+  // document.querySelector(`.message`).textContent = `Start guessing...`;
   document.querySelector(`.number`).style.width = `15rem`;
   document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.number`).textContent = `?`;
