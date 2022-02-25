0914f797c161b64d0823ba01f886f4ac9c2f603d
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-25 13:07:23 +0200

    pig game
    building our own very first real web game

--- MODAL/script.js
+++ MODAL/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overLay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overLay.classList.remove(`hidden`);
+};
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overLay.classList.add(`hidden`);
+};
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+}
+
+overLay.addEventListener(`click`, closeModal);
+modal.addEventListener(`click`, closeModal);
+document.addEventListener(`keydown`, function (e) {
+  console.log(e);
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
