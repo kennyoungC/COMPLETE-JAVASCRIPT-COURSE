801b8cb7523e15eafb2479cfbdd485850dfb6e72
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-01 21:00:59 +0200

    challenge completed

--- 07- Data-Structures-Operators/script.js
+++ 07- Data-Structures-Operators/script.js
@@ -196,10 +196,59 @@ console.log(undefined || null);
 
 console.log(undefined || 0 || ` ` || `hello` || 23 || null);
 
-restaurant.numGuests = 23;
+restaurant.numGuests = 0;
 const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guest1);
 
 const guest2 = restaurant.numGuests || 10;
 console.log(guest2);
+////////////nullish coalacing operator
+//nullish
+const guest3 = restaurant.numGuests ?? 10;
+
+console.log(guest3);
+/////////////////////////////////////////////
 console.log(`---------- AND ---------`);
+console.log(0 && `jonas`);
+console.log(0 && `jonas`);
+console.log(7 && `jonas`);
+
+console.log(`hello` && 23 && null && `jonas`);
+
+//practical example
+if (restaurant.orderpizza) {
+  restaurant.order(`mushroom`, `spinach`);
+}
+
+restaurant.orderpizza && restaurant.orderpizza(`mushroom`, `spinach`);
+
+const rest1 = {
+  name: `paul`,
+  numGuests: 20,
+};
+const rest2 = {
+  name: `paul`,
+  owner: `Ken castle`,
+};
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10
+// rest2.numGuests = rest2.numGuests || 10
+
+// rest1.numGuests ||= 10
+// rest2.numGuests ||= 10
+
+// nullish assignment operator(null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+//AND assignment operator
+// rest1.owner = rest1.numGuests && `<ANNOYMOUS>`;
+// rest2.owner = rest2.numGuests && `<ANNOYMOUS>`;
+
+rest1.owner &&= `<ANNOYMOUS>`;
+rest2.owner &&= `<ANNOYMOUS>`;
+
+console.log(rest1);
+console.log(rest2);
