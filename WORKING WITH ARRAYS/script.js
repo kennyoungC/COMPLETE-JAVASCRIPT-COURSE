608886e2af885591a30e98e966375b424dcee1e0
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-03 23:11:36 +0200

    array methods

--- WORKING WITH ARRAYS/script.js
+++ WORKING WITH ARRAYS/script.js
@@ -65,19 +65,13 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
 let arr = [`a`, `b`, `c`, `d`, `e`];
 
-//SLICE METHOD
+//SLICE METHOD - doesnt mutate only return value
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
 console.log(arr.slice(-2));
@@ -88,4 +82,68 @@ console.log(arr.slice(1, -2));
 console.log(arr.slice());
 console.log([...arr]);
 
-//SPLICE
+//SPLICE- mutate
+// console.log(arr.splice(2));
+//second parameter is the delete count
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//REVERSE - mutate
+arr = [`a`, `b`, `c`, `d`, `e`];
+const arr2 = [`j`, `i`, `h`, `g`, `f`];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+//same as
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join(`-`));
+///AT
+const arr3 = [23, 11, 64];
+console.log(arr3[0]);
+console.log(arr3.at(0));
+
+//getting last element of an array
+console.log(arr3[arr3.length - 1]);
+console.log(arr3.slice(-1)[0]);
+console.log(arr3.at(-1));
+//also works on strings
+
+console.log(`jonas`.at(0));
+console.log(`jonas`.at(-1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log(`----------FOR EACH-----------`);
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: finction(200)
+// 0: finction(450)
+// 0: finction(400)
+// ...
+// FOR EACH ON A MAP
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
