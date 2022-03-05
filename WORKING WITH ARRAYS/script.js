fa9e2c0ecad88d93d5ec8b6d2a513792ecb088fc
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-05 11:32:40 +0200

    some and every method

--- WORKING WITH ARRAYS/script.js
+++ WORKING WITH ARRAYS/script.js
@@ -171,6 +171,48 @@ btnTransfer.addEventListener(`click`, function (e) {
   //UPDATE UI
   updateUI(currentAccount);
 });
+
+btnLoan.addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    //ADD MOVEMENT
+    currentAccount.movements.push(amount);
+
+    // UPDATE UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = ``;
+});
+
+btnClose.addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) == currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+
+    inputCloseUsername.value = inputClosePin.value = ``;
+
+    labelWelcome.textContent = `Log in to get started`;
+  }
+});
 /////////////////////////////////////////////////
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
@@ -330,3 +372,21 @@ currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
 */
+/////////////////////////////////////
+// SOME
+// EQUALITY
+console.log(movements);
+console.log(movements.includes(-130));
+// CONDITION
+console.log(movements.some((mov) => mov === -130));
+const anyDeposits = movements.some((mov) => mov > 1500);
+console.log(anyDeposits);
+////////////////////////////////////
+// EVERY
+console.log(movements.every((mov) => mov > 0));
+console.log(account4.movements.every((mov) => mov > 0));
+// seperate call back
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
