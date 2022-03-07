d4c934b9637c2273c3b835bb878cc72d196aa4ee
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-08 00:12:03 +0200

    checking for a number

--- numbers and dates/script.js
+++ numbers and dates/script.js
@@ -261,3 +261,32 @@ console.log(0.1 + 0.2 === 0.3);
 // Conversion
 console.log(Number(`23`));
 console.log(+`23`);
+
+// Parsing
+console.log(Number.parseInt(`30px`, 10));
+console.log(Number.parseInt(`e23`, 10));
+
+console.log(Number.parseInt(`   2.5rem   `));
+console.log(Number.parseFloat(`   2.5rem   `));
+
+//console.log(parseFloat(`    2.5rem   `))
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(`20`));
+console.log(Number.isNaN(`20X`));
+console.log(Number.isNaN(23 / 0));
+
+// Check if value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(`20`));
+console.log(Number.isNaN(+`X`));
+console.log(Number.isNaN(23 / 0));
+
+// Checking if a value is number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite(`20`));
+console.log(Number.isFinite(+`20X`));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
