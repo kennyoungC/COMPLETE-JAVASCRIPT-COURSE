9ed3d3d2d55680a753a85240417b8c36cacd6cfc
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-08 17:26:42 +0200

    the remainder operator

--- numbers and dates/script.js
+++ numbers and dates/script.js
@@ -328,3 +328,30 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// The remainder operator
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 / 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener(`click`, function () {
+  [...document.querySelectorAll(`.movements__row`)].forEach(function (row, i) {
+    //0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = `orangered`;
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = `blue`;
+  });
+});
