37bccb7d249376cf40254c0c42bbc3921d34086a
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-09 19:33:01 +0200

    Event Propagation in Practice

--- Advanced-Dom-And-Events/script.js
+++ Advanced-Dom-And-Events/script.js
@@ -125,3 +125,28 @@ const alertH1 = function (e) {
 h1.addEventListener(`mouseenter`, alertH1);
 
 setTimeout(() => h1.removeEventListener(`mouseenter`, alertH1), 5000);
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+console.log(randomInt(0, 10));
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector(`.nav__link`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(`LINK`, e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  //stop  propagation
+  // e.stopPropagation()
+});
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(`CONTAINER`, e.target, e.currentTarget);
+});
+document.querySelector(`.nav`).addEventListener(`click`, function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log(`NAV`, e.target, e.currentTarget);
+});
